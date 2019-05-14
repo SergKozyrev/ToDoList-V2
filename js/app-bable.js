34c835abbba9c1647b47395bb4a3356b95748947
Sergey Kozyrev <serhii.kozyriev@gmail.com>
2019-05-14 16:46:47 +0300

    polyfill

--- js/app-bable.js
+++ js/app-bable.js
@@ -1,18 +1,18 @@
 "use strict";
 
-require("core-js/modules/es6.string.iterator");
+import "core-js/modules/es6.string.iterator";
 
-require("core-js/modules/es6.array.from");
+import "core-js/modules/es6.array.from";
 
-require("core-js/modules/es6.regexp.to-string");
+import "core-js/modules/es6.regexp.to-string";
 
-require("core-js/modules/es6.object.to-string");
+import "core-js/modules/es6.object.to-string";
 
-require("core-js/modules/es7.symbol.async-iterator");
+import "core-js/modules/es7.symbol.async-iterator";
 
-require("core-js/modules/es6.symbol");
+import "core-js/modules/es6.symbol";
 
-require("core-js/modules/web.dom.iterable");
+import "core-js/modules/web.dom.iterable";
 
 function _toConsumableArray(arr) {
   return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread();
